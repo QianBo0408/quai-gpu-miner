ae0b76f8999842d119e4236301a446e31248cfbd
QianBo0408 <79657605+QianBo0408@users.noreply.github.com>
2023-10-23 17:10:35 +0800

    tes
    2222

---
+++ quai14.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 更新和升级Ubuntu软件包
+sudo apt update && sudo apt upgrade -y
+
+# 安装必要的软件包
+sudo apt install -y git cmake build-essential mesa-common-dev screen nano
+
+# 克隆指定的git仓库并进入相应目录
+git clone https://github.com/DD000oo/quai-gpu-miner.git && cd quai-gpu-miner
+
+
+
+# 更新git子模块
+git submodule update --init --recursive
+
+echo "-------------------------"
+echo -e "\e[31mCurrent Directory: $(pwd)\e[0m"
+echo "-------------------------"
+
+# 创建build目录并进入
+mkdir build && cd build
+
+
+# 运行cmake命令
+cmake .. && cmake --build .
+cd ..
+
+
+echo "-------------------------"
+echo -e "\e[31mCurrent Directory: $(pwd)\e[0m"
+echo "-------------------------"
+
+
+# 创建test.sh文件
+echo "#!/bin/bash
+while [ 1 ];
+do
+    sleep 2
+    ./ethcoreminer/ethcoreminer -P stratum://47.253.41.254:3333 -L 1 && break
+done" > test.sh
+
+# 使test.sh可执行
+chmod +x test.sh
+
+# 使用screen命令
+screen -R gpu
